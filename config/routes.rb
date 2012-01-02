e19461767b38bd1df21787afdfd0024afe1fa9fc
davidkuperman <kuper_7@hotmail.com>
2012-01-02 12:49:51 -0500

    cambios en el root

--- config/routes.rb
+++ config/routes.rb
@@ -60,7 +60,7 @@ Prueba15::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => "ceos#index"
+   root :to => "pages#home"
 
   # See how all your routes lay out with "rake routes"
 
