b7038678e72d6c3f39ab17ceee1a47514d987766
davidkuperman <kuper_7@hotmail.com>
2012-01-02 12:33:59 -0500

    2enero

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,14 @@
 class PagesController < ApplicationController
   def home
+    @title = "Home"
   end
 
   def about
+    @title = "About"
   end
 
   def pricing
-    
+    @title = "Pricing"
   end
 
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -7,6 +7,11 @@ describe "GET 'home'" do
    get 'home'
    response.should be_success
  end
+
+ it "should have the right title" do
+   get 'home'
+   response.should have_selector(:title, :content => "ROR Home")
+ end
 end
 
 describe "GET 'about'" do
@@ -14,12 +19,22 @@ describe "GET 'about'" do
     get 'about'
     response.should be_success
   end
+
+  it "should have the right title" do
+    get 'about'
+    response.should have_selector(:title, :content => "ROR About")
+  end
 end
 
   describe "GET 'pricing'" do
     it "should be succesfull" do
       get 'pricing'
       response.should be_success
+    end
+
+    it "should have the rigth title" do
+      get 'pricing'
+      response.should have_selector(:title, :content => "ROR Pricing")
     end
   end
 
